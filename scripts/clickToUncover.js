063eb365e2d2da85da6006e73d877c304d579f48
Antoine V <antoine.verove@gmail.com>
2024-11-12 11:29:41 +0100

    fix the clicktouncover issue

--- scripts/clickToUncover.js
+++ scripts/clickToUncover.js
@@ -10,28 +10,17 @@ function clickToUncover() {
 }
 
 function onClick(e) {
-    e.preventDefault()
-    grecaptcha.ready(function () {
-        grecaptcha
-            .execute('6LdQu3kqAAAAAMR21RJ5KddypGkJjJB3qYViGGYv', {
-                action: 'submit',
-            })
-            .then(function (token) {
-                // Add your logic to submit to your backend server here.
-                if (e.classList.contains('phone')) {
-                    e.innerHTML = ''
-                    e.innerHTML =
-                        "<a href='tel:+33637335510'>06 37 33 55 10</a>"
-                }
-                if (e.classList.contains('mail')) {
-                    e.innerHTML = ''
-                    e.innerHTML = `
+    if (e.classList.contains('phone')) {
+        e.innerHTML = ''
+        e.innerHTML = "<a href='tel:+33637335510'>06 37 33 55 10</a>"
+    }
+    if (e.classList.contains('mail')) {
+        e.innerHTML = ''
+        e.innerHTML = `
 <a style="margin='auto'" href="mailto:lisatastevin.psychomot@gmail.com?subject=Demande%20d'information&body=Bonjour,%20je%20souhaite%20plus%20d'informations.">
 lisatastevin.psychomot@gmail.com
 </a>`
-                }
-            })
-    })
+    }
 }
 
 clickToUncover()
