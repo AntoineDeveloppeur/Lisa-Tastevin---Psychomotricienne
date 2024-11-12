b86c7a46d36169467d4c4dbfc54ec2ca19da7636
Antoine V <antoine.verove@gmail.com>
2024-11-12 12:06:52 +0100

    change la version de click toUncover à celle de main avant l'implementation du captcha

--- scripts/clickToUncover.js
+++ scripts/clickToUncover.js
@@ -3,24 +3,25 @@ function clickToUncover() {
         '.OuMeTrouver__flexbox1__bloc1__ul__li__clickToUncover'
     )
     elementToUncover.forEach((element) => {
-        element.addEventListener('click', () => onClick(element), {
-            once: true,
-        })
+        element.addEventListener(
+            'click',
+            () => {
+                if (element.classList.contains('phone')) {
+                    element.innerHTML = ''
+                    element.innerHTML =
+                        "<a href='tel:+33637335510'>06 37 33 55 10</a>"
+                }
+                if (element.classList.contains('mail')) {
+                    element.innerHTML = ''
+                    element.innerHTML = `
+    <a style="margin='auto'" href="mailto:lisatastevin.psychomot@gmail.com?subject=Demande%20d'information&body=Bonjour,%20je%20souhaite%20plus%20d'informations.">
+        lisatastevin.psychomot@gmail.com
+    </a>`
+                }
+            },
+            { once: true }
+        )
     })
-}
-
-function onClick(e) {
-    if (e.classList.contains('phone')) {
-        e.innerHTML = ''
-        e.innerHTML = "<a href='tel:+33637335510'>06 37 33 55 10</a>"
-    }
-    if (e.classList.contains('mail')) {
-        e.innerHTML = ''
-        e.innerHTML = `
-<a style="margin='auto'" href="mailto:lisatastevin.psychomot@gmail.com?subject=Demande%20d'information&body=Bonjour,%20je%20souhaite%20plus%20d'informations.">
-lisatastevin.psychomot@gmail.com
-</a>`
-    }
 }
 
 clickToUncover()
