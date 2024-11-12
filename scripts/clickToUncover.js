a66b974d5d3100a44e3be6aa05474bc806d56867
Antoine V <antoine.verove@gmail.com>
2024-11-12 10:58:50 +0100

    nouvelle branch pourProduction

--- scripts/clickToUncover.js
+++ scripts/clickToUncover.js
@@ -3,24 +3,34 @@ function clickToUncover() {
         '.OuMeTrouver__flexbox1__bloc1__ul__li__clickToUncover'
     )
     elementToUncover.forEach((element) => {
-        element.addEventListener(
-            'click',
-            () => {
-                if (element.classList.contains('phone')) {
-                    element.innerHTML = ''
-                    element.innerHTML =
+        element.addEventListener('click', () => onClick(element), {
+            once: true,
+        })
+    })
+}
+
+function onClick(e) {
+    e.preventDefault()
+    grecaptcha.ready(function () {
+        grecaptcha
+            .execute('6LdQu3kqAAAAAMR21RJ5KddypGkJjJB3qYViGGYv', {
+                action: 'submit',
+            })
+            .then(function (token) {
+                // Add your logic to submit to your backend server here.
+                if (e.classList.contains('phone')) {
+                    e.innerHTML = ''
+                    e.innerHTML =
                         "<a href='tel:+33637335510'>06 37 33 55 10</a>"
                 }
-                if (element.classList.contains('mail')) {
-                    element.innerHTML = ''
-                    element.innerHTML = `
-    <a style="margin='auto'" href="mailto:lisatastevin.psychomot@gmail.com?subject=Demande%20d'information&body=Bonjour,%20je%20souhaite%20plus%20d'informations.">
-        lisatastevin.psychomot@gmail.com
-    </a>`
+                if (e.classList.contains('mail')) {
+                    e.innerHTML = ''
+                    e.innerHTML = `
+<a style="margin='auto'" href="mailto:lisatastevin.psychomot@gmail.com?subject=Demande%20d'information&body=Bonjour,%20je%20souhaite%20plus%20d'informations.">
+lisatastevin.psychomot@gmail.com
+</a>`
                 }
-            },
-            { once: true }
-        )
+            })
     })
 }
 
