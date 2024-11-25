a5934e6d5107f6fd72c694d24cb4e75be630ab3e
Antoine V <antoine.verove@gmail.com>
2024-11-25 18:30:46 +0100

    Changement adresse de cross origin pour tester le CORS

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(express.json())
 app.use((req, res, next) => {
     res.setHeader(
         'Access-Control-Allow-Origin',
-        '*'
+        '46.200.123.123'
         // 'https://lisa-tastevin-psychomotricienne.fr'
     )
     res.setHeader(
