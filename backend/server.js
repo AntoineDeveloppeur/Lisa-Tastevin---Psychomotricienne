985fc38c11b9cda3bf5293eef378458438e2241b
Antoine V <antoine.verove@gmail.com>
2024-12-04 10:36:36 +0100

    debuggage en cours

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(express.json())
 app.use((req, res, next) => {
     res.setHeader(
         'Access-Control-Allow-Origin',
-        'http://127.0.0.1:5501'
+        '*'
         // 'https://lisa-tastevin-psychomotricienne.fr'
     )
     res.setHeader(
